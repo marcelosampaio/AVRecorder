73a62a33273cfa84815f22d938caf5783ada8b16
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2023-05-12 13:40:09 -0300

    recorder steps 🎙️

--- AVRecorder/Controller/ViewController.swift
+++ AVRecorder/Controller/ViewController.swift
@@ -1,20 +1,80 @@
-//
-//  ViewController.swift
-//  AVRecorder
-//
-//  Created by Marcelo Sampaio on 12/05/23.
-//
-
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+    // MARK: - Outlets
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .brown
+        // 📍 prepare AV
+        recordingSession = AVAudioSession.sharedInstance()
+
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.loadRecordingUI()
+                    } else {
+                        print("❌ no permission granted")
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+            print("❌ failed to record")
+        }
+    }
+    
+    func loadRecordingUI() {
+        recordButton.setTitle("Tap to Record", for: .normal)
+        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        if audioRecorder == nil {
+            startRecording()
+            print("🎙️ record started")
+        }
+    }
+    
+    @IBAction func stopRecord(_ sender: Any) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        print("🎙️ record stopped")
     }
 
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.mp4")
 
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+
+            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            print("❌ error starting record")
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        print("path: \(paths[0].absoluteString)")
+        return paths[0]
+    }
 }
-
